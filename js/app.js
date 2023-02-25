679a8dfa92a04822f8db3ee1eabbc082793f5768
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-02-25 14:53:18 -0800

    OOP JS file update

--- js/app.js
+++ js/app.js
@@ -2,62 +2,7 @@ function randomNumbers(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-
-function render(shop) {
-  let table = document.createElement("table");
-
-  let headerRow = document.createElement("tr");
-  let locationHeader = document.createElement("th");
-  locationHeader.textContent = "Location";
-  let minCustomersHeader = document.createElement("th");
-  minCustomersHeader.textContent = "Min / Cust";
-  let maxCustomersHeader = document.createElement("th");
-  maxCustomersHeader.textContent = "Max / Cust";
-  let avgCookiesHeader = document.createElement("th");
-  avgCookiesHeader.textContent = "Avg Cookie / Sale";
-  headerRow.appendChild(locationHeader);
-  headerRow.appendChild(minCustomersHeader);
-  headerRow.appendChild(maxCustomersHeader);
-  headerRow.appendChild(avgCookiesHeader);
-  table.appendChild(headerRow);
-
-  let dataRow = document.createElement("tr");
-  let locationCell = document.createElement("td");
-  locationCell.textContent = shop.location;
-  let minCustomersCell = document.createElement("td");
-  minCustomersCell.textContent = shop.minCustomers;
-  let maxCustomersCell = document.createElement("td");
-  maxCustomersCell.textContent = shop.maxCustomers;
-  let avgCookiesCell = document.createElement("td");
-  avgCookiesCell.textContent = shop.avgCookies;
-  dataRow.appendChild(locationCell);
-  dataRow.appendChild(minCustomersCell);
-  dataRow.appendChild(maxCustomersCell);
-  dataRow.appendChild(avgCookiesCell);
-  table.appendChild(dataRow);
-
-  document.body.appendChild(table);
-}
-
-function renderArray(shop) {
-  let hourlySalesList = document.createElement("ul");
-
-  for (let i = 6; i < 19; i++) {
-    let customers = randomNumbers(shop.minCustomers, shop.maxCustomers);
-    let cookies = Math.round(customers * shop.avgCookies);
-    shop.hourlySales.push(cookies);
-
-    if (i <= 12) {
-      let listItem = document.createElement("li");
-      listItem.textContent = `${i}:00am - ${cookies} cookies sold`;
-      hourlySalesList.appendChild(listItem);
-    } else {
-      let listItem = document.createElement("li");
-      listItem.textContent = `${i}:00pm - ${cookies} cookies sold`;
-      hourlySalesList.appendChild(listItem);
-    }
-
-class Shop {
+class CookieShop {
   constructor(location, minCustomers, maxCustomers, avgCookies) {
     this.location = location;
     this.minCustomers = minCustomers;
@@ -66,7 +11,6 @@ class Shop {
     this.hourlySales = [];
     this.dailyTotal = 0;
     this.generateHourlySales();
-
   }
 
   generateHourlySales() {
@@ -97,48 +41,6 @@ class Shop {
   }
 }
 
-
-let shopOne = {
-  location: "Seattle",
-  minCustomers: 23,
-  maxCustomers: 65,
-  avgCookies: 6.3,
-  hourlySales: [],
-};
-
-render(shopOne);
-renderArray(shopOne);
-
-let shopTwo = {
-  location: "Tokyo",
-  minCustomers: 3,
-  maxCustomers: 24,
-  avgCookies: 1.2,
-  hourlySales: [],
-};
-
-render(shopTwo);
-renderArray(shopTwo);
-
-let shopThree = {
-  location: "Dubai",
-  minCustomers: 11,
-  maxCustomers: 38,
-  avgCookies: 3.7,
-  hourlySales: [],
-};
-
-render(shopThree);
-renderArray(shopThree);
-
-let shopFour = {
-  location: "Paris",
-  minCustomers: 20,
-  maxCustomers: 38,
-  avgCookies: 2.3,
-  hourlySales: [],
-};
-
 let table = document.createElement("table");
 let headerRow = document.createElement("tr");
 let locationHeader = document.createElement("th");
@@ -160,27 +62,16 @@ dailyTotalHeader.textContent = "Daily Location Total";
 headerRow.appendChild(dailyTotalHeader);
 table.appendChild(headerRow);
 
-
-let shopOne = new Shop("Seattle", 2, 8, 10);
-let shopTwo = new Shop("Tokyo", 3, 10, 15);
-let shopThree = new Shop("Dubai", 1, 4, 20);
-let shopFour = new Shop("Paris", 5, 7, 25);
-let shopFive = new Shop("Lima", 1, 5, 50);
-
-
-let shopFive = {
-  location: "Lima",
-  minCustomers: 2,
-  maxCustomers: 16,
-  avgCookies: 4.6,
-  hourlySales: [],
-};
+let shopOne = new CookieShop("Seattle", 20, 55, 10);
+let shopTwo = new CookieShop("Tokyo", 30, 45, 15);
+let shopThree = new CookieShop("Dubai", 1, 25, 20);
+let shopFour = new CookieShop("Paris", 5, 15, 25);
+let shopFive = new CookieShop("Lima", 1, 25, 50);
 
 shopOne.render(table);
 shopTwo.render(table);
 shopThree.render(table);
 shopFour.render(table);
 shopFive.render(table);
-
 
 document.body.appendChild(table);
