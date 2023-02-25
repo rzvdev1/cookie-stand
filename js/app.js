e3ff2b738161031b12aa842690d72d711b76ce41
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-02-25 13:46:06 -0800

    style table and OOP js

--- js/app.js
+++ js/app.js
@@ -2,147 +2,76 @@ function randomNumbers(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function render(shop) {
-  let table = document.createElement("table");
+class Shop {
+  constructor(location, minCustomers, maxCustomers, avgCookies) {
+    this.location = location;
+    this.minCustomers = minCustomers;
+    this.maxCustomers = maxCustomers;
+    this.avgCookies = avgCookies;
+    this.hourlySales = [];
+    this.dailyTotal = 0;
+    this.generateHourlySales();
+  }
 
-  let headerRow = document.createElement("tr");
-  let emptyCell = document.createElement("th");
-  headerRow.appendChild(emptyCell);
-
-  // let headerRow = document.createElement("tr");
-  // let locationHeader = document.createElement("th");
-  // locationHeader.textContent = "Location";
-  // let minCustomersHeader = document.createElement("th");
-  // minCustomersHeader.textContent = "Min / Cust";
-  // let maxCustomersHeader = document.createElement("th");
-  // maxCustomersHeader.textContent = "Max / Cust";
-  // let avgCookiesHeader = document.createElement("th");
-  // avgCookiesHeader.textContent = "Avg Cookie / Sale";
-  // headerRow.appendChild(locationHeader);
-  // headerRow.appendChild(minCustomersHeader);
-  // headerRow.appendChild(maxCustomersHeader);
-  // headerRow.appendChild(avgCookiesHeader);
-  // table.appendChild(headerRow);
-
-  // let dataRow = document.createElement("tr");
-  // let locationCell = document.createElement("td");
-  // locationCell.textContent = shop.location;
-  // let minCustomersCell = document.createElement("td");
-  // minCustomersCell.textContent = shop.minCustomers;
-  // let maxCustomersCell = document.createElement("td");
-  // maxCustomersCell.textContent = shop.maxCustomers;
-  // let avgCookiesCell = document.createElement("td");
-  // avgCookiesCell.textContent = shop.avgCookies;
-  // dataRow.appendChild(locationCell);
-  // dataRow.appendChild(minCustomersCell);
-  // dataRow.appendChild(maxCustomersCell);
-  // dataRow.appendChild(avgCookiesCell);
-  // table.appendChild(dataRow);
-
-  document.body.appendChild(table);
-}
-
-function renderArray(shop) {
-  let hourlySalesTable = document.createElement("table");
-
-  let headerRow = document.createElement("tr");
-
-  let emptyCell = document.createElement("th");
-  headerRow.appendChild(emptyCell);
-
-  // stackoverflow time format 24 hours to 12 : https://stackoverflow.com/questions/4898574/converting-24-hour-time-to-12-hour-time-w-am-pm-using-javascript
-  for (let i = 6; i < 20; i++) {
-    let hourHeader = document.createElement("th");
-    if (i <= 12) {
-      hourHeader.textContent = i + ":00am";
-    } else {
-      hourHeader.textContent = i - 12 + ":00pm";
+  generateHourlySales() {
+    for (let i = 6; i < 20; i++) {
+      let customers = randomNumbers(this.minCustomers, this.maxCustomers);
+      let cookies = Math.round(customers * this.avgCookies);
+      this.hourlySales.push(cookies);
+      this.dailyTotal += cookies;
     }
-    headerRow.appendChild(hourHeader);
   }
-  let dailyTotalHeader = document.createElement("th");
-  dailyTotalHeader.textContent = "Daily Location Total";
-  headerRow.appendChild(dailyTotalHeader);
-  hourlySalesTable.appendChild(headerRow);
 
-  let dataRow = document.createElement("tr");
-  let locationCell = document.createElement("td");
-  locationCell.textContent = shop.location;
-  dataRow.appendChild(locationCell);
-  let dailyTotal = 0;
-  for (let i = 6; i < 20; i++) {
-    let customers = randomNumbers(shop.minCustomers, shop.maxCustomers);
-    let cookies = Math.round(customers * shop.avgCookies);
-    shop.hourlySales.push(cookies);
-    let cookiesCell = document.createElement("td");
-    cookiesCell.textContent = cookies;
-    dataRow.appendChild(cookiesCell);
-    dailyTotal += cookies;
+  render(table) {
+    let dataRow = document.createElement("tr");
+    let locationCell = document.createElement("td");
+    locationCell.textContent = this.location;
+    dataRow.appendChild(locationCell);
+    for (let i = 0; i < this.hourlySales.length; i++) {
+      let cookiesCell = document.createElement("td");
+      cookiesCell.textContent = this.hourlySales[i];
+      dataRow.appendChild(cookiesCell);
+    }
+
+    let dailyTotalCell = document.createElement("td");
+    dailyTotalCell.textContent = this.dailyTotal;
+    dataRow.appendChild(dailyTotalCell);
+
+    table.appendChild(dataRow);
   }
-  let dailyTotalCell = document.createElement("td");
-  dailyTotalCell.textContent = dailyTotal;
-  dataRow.appendChild(dailyTotalCell);
-  hourlySalesTable.appendChild(dataRow);
-
-  document.body.appendChild(hourlySalesTable);
 }
 
-let shopOne = {
-  location: "Seattle",
-  minCustomers: randomNumbers(20, 55),
-  maxCustomers: randomNumbers(30, 55),
-  avgCookies: randomNumbers(10, 55),
-  hourlySales: [],
-};
+let table = document.createElement("table");
+let headerRow = document.createElement("tr");
+let locationHeader = document.createElement("th");
+locationHeader.textContent = "Location";
+headerRow.appendChild(locationHeader);
+// stackoverflow time format 24 hours to 12 : https://stackoverflow.com/questions/4898574/converting-24-hour-time-to-12-hour-time-w-am-pm-using-javascript
+for (let i = 6; i < 20; i++) {
+  let hourHeader = document.createElement("th");
+  if (i <= 12) {
+    hourHeader.textContent = i + ":00am";
+  } else {
+    hourHeader.textContent = i - 12 + ":00pm";
+  }
+  headerRow.appendChild(hourHeader);
+}
 
-render(shopOne);
+let dailyTotalHeader = document.createElement("th");
+dailyTotalHeader.textContent = "Daily Location Total";
+headerRow.appendChild(dailyTotalHeader);
+table.appendChild(headerRow);
 
-renderArray(shopOne);
+let shopOne = new Shop("Seattle", 2, 8, 10);
+let shopTwo = new Shop("Tokyo", 3, 10, 15);
+let shopThree = new Shop("Dubai", 1, 4, 20);
+let shopFour = new Shop("Paris", 5, 7, 25);
+let shopFive = new Shop("Lima", 1, 5, 50);
 
-let shopTwo = {
-  location: "Tokyo",
-  minCustomers: randomNumbers(30, 45),
-  maxCustomers: randomNumbers(12, 25),
-  avgCookies: randomNumbers(10, 20),
-  hourlySales: [],
-};
+shopOne.render(table);
+shopTwo.render(table);
+shopThree.render(table);
+shopFour.render(table);
+shopFive.render(table);
 
-render(shopTwo);
-
-renderArray(shopTwo);
-
-let shopThree = {
-  location: "Dubai",
-  minCustomers: randomNumbers(1, 25),
-  maxCustomers: randomNumbers(15, 25),
-  avgCookies: randomNumbers(10, 35),
-  hourlySales: [],
-};
-
-render(shopThree);
-
-renderArray(shopThree);
-
-let shopFour = {
-  location: "Paris",
-  minCustomers: randomNumbers(5, 15),
-  maxCustomers: randomNumbers(30, 35),
-  avgCookies: randomNumbers(10, 30),
-  hourlySales: [],
-};
-
-render(shopFour);
-
-renderArray(shopFour);
-
-let shopFive = {
-  location: "Lima",
-  minCustomers: randomNumbers(1, 25),
-  maxCustomers: randomNumbers(60, 65),
-  avgCookies: randomNumbers(45, 55),
-  hourlySales: [],
-};
-
-render(shopFive);
-
-renderArray(shopFive);
+document.body.appendChild(table);
